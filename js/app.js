f20c5b465b19c89203da5dea7dce24a93c1de761
John Nieves <karn09@gmail.com>
2015-12-07 23:03:12 -0500

    added clear bindings function. added lowerOrHigher value check. added maxGuesses check

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 
 // DONE - When a game begins, there should be a random number generated between 1-100.
 // DONE - The user should have an input field where they can submit a guess.
-// After the user submits a guess, indicate whether their guess is 'hot' or 'cold'. Let the user know if they need to guess higher or lower.
-// Allow the user to guess only a certain amount of times. When they run out of guesses let them know the game is over.
+// DONE After the user submits a guess, indicate whether their guess is 'hot' or 'cold'. Let the user know if they need to guess higher or lower.
+// DONE Allow the user to guess only a certain amount of times. When they run out of guesses let them know the game is over.
 // DONE - Feel free to use prompts to get user input on your first version.
 // DONE - For the final version of your project, you'll need to create an HTML-based interface for getting user inputs and giving feedback on guesses.
 // DONE - Validate inputs that they are real numbers between 1-100.
@@ -14,6 +14,14 @@
 // DONE - Submit the guess by pressing enter or clicking the submit button.
 // DONE (make this look nicer) - After a user guesses a number keep a visual list of Hot and Cold answers that the user can see.
 // Change the background color, add an image, or do something creative when the user guesses the correct answer.
+
+
+// TODO:
+// create and display array of hints depending on guesses left. 
+// if winning number found, stop accepting additional input
+// if no more guesses availible, stop accepting additional input
+// display numbers previously guessed - maybe for numbers > x digits away- show in cold panel. as gets closer- show in hot panel
+// cool winning panel popup? 
 
 jQuery(function($) {
     var Model = {
@@ -33,6 +41,8 @@ jQuery(function($) {
             this.cacheElements();
             this.bindEvents();
             this.render();
+            this.guesses = 0;
+            this.maxGuess = 5;
         },
         cacheElements: function() {
             this.$hot = $('.hot');
@@ -41,24 +51,38 @@ jQuery(function($) {
             this.$guessForm = $("#guessForm");
             this.$guessBtn = $('#getGuess');
             this.$resetBtn = $('#reset');
+            this.$guessInput = $('#guess');
             this.$answerBtn = $('#answer');
             this.$coldWell = $('.cold-well');
             this.$hotWell = $('.hot-well');
             this.$tryAgain = $('.tryAgain');
             this.$tryMsg = $('#tryMsg');
         },
+        // on reset, clear out all bindings so that multiple clicks aren't accidentally registered.
+        clearEvents: function () {
+            this.$guessBtn.unbind();
+            this.$guessInput.unbind();
+            this.$resetBtn.unbind();
+            this.$answerBtn.unbind();
+        },
         bindEvents: function() {
             // encountered an issue where bindings were firing on 1,2,4,8,16..
             //http://stackoverflow.com/questions/14969960/jquery-click-events-firing-multiple-times
             var self = this;
-            this.$guessBtn.one().click(function() {
-                var guessInput = $('#guess');
-                self.checkInput(guessInput.val())
+            this.$guessBtn.click(function() {
+                var currentVal = $('#guess');
+                self.checkInput(currentVal.val());
             });
-            this.$resetBtn.one().click(function() {
+            this.$guessInput.keypress(function(e) {
+                if (e.keyCode === 13) {
+                    var currentVal = $('#guess');
+                    self.checkInput(currentVal.val());
+                }
+            })
+            this.$resetBtn.click(function() {
                 self.resetGame();
             });
-            this.$answerBtn.one().click(function() {
+            this.$answerBtn.click(function() {
                 self.getAnswer();
             })
         },
@@ -95,14 +119,22 @@ jQuery(function($) {
         checkInput: function(val) {
             var self = this;
             val = Number(val);
-            if (typeof val == "number" && Math.floor(val) === val) {
-                if (val < 1 || val > 100) {
-                    self.chgMsg("Between 1 and 100 only!");
+            if (this.guesses === this.maxGuess) {
+                this.chgMsg("Maybe it's time you gave up and started a new game.")
+            } else {                
+                if (typeof val == "number" && Math.floor(val) === val) {
+                    if (val < 1 || val > 100) {
+                        self.chgMsg("Between 1 and 100 only!");
+                    } else {
+                        if (this.guesses < this.maxGuess) {
+                            this.checkGuess(val);
+                            this.guesses++;
+                            console.log(this.guesses);
+                        }
+                    }
                 } else {
-                    this.checkGuess(val);
+                    self.chgMsg('Decimal numbers only!');
                 }
-            } else {
-                self.chgMsg('Decimal numbers only!');
             }
         },
         // check input value for nearness to generated value and push to hot/cold obj arrays.
@@ -112,18 +144,34 @@ jQuery(function($) {
                 this.chgMsg("You found it! " + val) 
             } else if (this.prevGuess.hot.indexOf(val) !== -1 || this.prevGuess.cold.indexOf(val) !== -1) {
                 this.chgMsg("Why not guess a new number..")
-            } else if (val > (this.answer + this.radius) || val < (this.answer - this.radius)) {
-                this.prevGuess.cold.push(val)
-                this.chgMsg("Brrr....not so close. ")
-            } else if (val < (this.answer + this.radius) && val > (this.answer - this.radius)) {
-                this.prevGuess.hot.push(val)
-                this.chgMsg("Getting hotter..")
-                this.radius--; // if value is hotter, reduce guess radius 
+            } else {
+                this.chgMsg(this.lowerOrHigher(val))
             }
+            // call lowerOrHigher
+            // (val > (this.answer + this.radius) || val < (this.answer - this.radius)) {
+            //     this.prevGuess.cold.push(val)
+            //     this.chgMsg("Brrr....not so close. ")
+            // } else if (val < (this.answer + this.radius) && val > (this.answer - this.radius)) {
+            //     this.prevGuess.hot.push(val)
+            //     this.chgMsg("Getting hotter..")
+            //     this.radius--; // if value is hotter, reduce guess radius 
+            // }
+        },
+        // check if # is lower or higher.
+        lowerOrHigher: function(val) {
+            var resp = 'Your guess is ';
+            if (val < this.answer) {
+                resp += 'lower than the answer and ';
+            } else if (val > this.answer) {
+                resp += 'greater than the answer and ';
+            }
+            resp += 'within ' + Math.ceil(Math.abs(this.answer - val)/5)*5 + ' digits';
+            return resp;
         },
 
         // call init to reset game. 
         resetGame: function() {
+            this.clearEvents();
             this.init();
         },
         // set msg to final answer. 
